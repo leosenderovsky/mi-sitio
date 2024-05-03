1e252c2f4470899420c62cc940225c9877adb2af
leosenderovsky <l.a.senderovsky@gmail.com>
2024-05-03 11:14:30 -0300

    agregado formulario de contacto, relinkeo secciones y demás

---
+++ assets/js/mostrar.js
@@ -0,0 +1,26 @@
+document.getElementById("mostrar-cv-editor-es").onclick = function() {
+    var audiovisual = document.getElementById("cv-audiovisual-es");
+    if (audiovisual.style.display === "none") {
+        audiovisual.style.display = "flex";
+    } else {
+        audiovisual.style.display = "none";
+    }
+}
+
+document.getElementById("mostrar-cv-editor-en").onclick = function() {
+    var audiovisual = document.getElementById("cv-audiovisual-en");
+    if (audiovisual.style.display === "none") {
+        audiovisual.style.display = "flex";
+    } else {
+        audiovisual.style.display = "none";
+    }
+}
+
+document.getElementById("mostrar-cv-web-en").onclick = function() {
+    var audiovisual = document.getElementById("cv-web-en");
+    if (audiovisual.style.display === "none") {
+        audiovisual.style.display = "flex";
+    } else {
+        audiovisual.style.display = "none";
+    }
+}
--- assets/js/ocultar.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("ocultarAudiovisual").onclick = function() {
-    var audiovisual = document.getElementById("web");
-    if (audiovisual.style.display === "none") {
-        audiovisual.style.display = "block";
-    } else {
-        audiovisual.style.display = "none";
-    }
-}
