c2c7e4de236bd28ac26f9fb63a1a17b6d8941790
leosenderovsky <l.a.senderovsky@gmail.com>
2024-05-05 13:38:49 -0300

    cambios botones material de lectura docencia y cambio fondo hero area por escritorio

--- assets/js/mostrar.js
+++ assets/js/mostrar.js
@@ -5,6 +5,20 @@ document.getElementById("mostrar-cv-editor-es").onclick = function() {
     } else {
         audiovisual.style.display = "none";
     }
+
+    var audiovisual = document.getElementById("cv-audiovisual-en");
+    if (audiovisual.style.display === "flex") {
+        audiovisual.style.display = "none";
+    } else {
+        audiovisual.style.display = "none";
+    }
+
+    var audiovisual = document.getElementById("cv-web-en");
+    if (audiovisual.style.display === "flex") {
+        audiovisual.style.display = "none";
+    } else {
+        audiovisual.style.display = "none";
+    }
 }
 
 document.getElementById("mostrar-cv-editor-en").onclick = function() {
@@ -14,12 +28,40 @@ document.getElementById("mostrar-cv-editor-en").onclick = function() {
     } else {
         audiovisual.style.display = "none";
     }
+
+    var audiovisual = document.getElementById("cv-audiovisual-es");
+    if (audiovisual.style.display === "flex") {
+        audiovisual.style.display = "none";
+    } else {
+        audiovisual.style.display = "none";
+    }
+
+    var audiovisual = document.getElementById("cv-web-en");
+    if (audiovisual.style.display === "flex") {
+        audiovisual.style.display = "none";
+    } else {
+        audiovisual.style.display = "none";
+    }
 }
 
 document.getElementById("mostrar-cv-web-en").onclick = function() {
     var audiovisual = document.getElementById("cv-web-en");
     if (audiovisual.style.display === "none") {
         audiovisual.style.display = "flex";
+    } else {
+        audiovisual.style.display = "none";
+    }
+
+    var audiovisual = document.getElementById("cv-audiovisual-en");
+    if (audiovisual.style.display === "flex") {
+        audiovisual.style.display = "none";
+    } else {
+        audiovisual.style.display = "none";
+    }
+
+    var audiovisual = document.getElementById("cv-audiovisual-es");
+    if (audiovisual.style.display === "flex") {
+        audiovisual.style.display = "none";
     } else {
         audiovisual.style.display = "none";
     }
