5ba7392006feb532ee4b90d3928e1b7bb0eb55c8
l.a.senderovsky <l.a.senderovsky@gmail.com>
2024-05-27 17:04:55 +0000

    cambios animaciones y cambio de texto en boton via js

--- assets/js/change-text.js
+++ assets/js/change-text.js
@@ -1,53 +1,14 @@
-/**
- * Change the text of a button
- * @param {el} object HTMLElement: button to change text of
- * @param {dText} string: default text
- * @param {nText} string: new text
- **/
-function changeText(el, dText, nText) {
-    var content = '',
-        context = '';
-    
-    /**
-     * Set the text of a button
-     *     - dependant upon current text
-     **/
-    function setText() {
-      return (content === dText) ? nText : dText;
-    }
-    
-    /* Check to see if the browser accepts textContent */
-    if ('textContent' in document.body) {
-      context = 'textContent';
-      /* Get the current button text */
-      content = el[context];
-    /* Browser does NOT use textContent */
-    } else {
-      /* Get the button text with fallback option */
-      content = el.firstChild.nodeValue;
-    }
-    
-    /* Set button text */
-    if (context === 'textContent') {
-      el.textContent = setText();
-    } else {
-      el.firstChild.nodeValue = setText();
-    }
-  }
-  
-  var defaultText,
-      substituteText,
-      btn;
-  
-  /* Default text of the button */
-  defaultText = 'Ver mi experiencia anterior';
-  /* Alternate text for button */
-  substituteText = 'Ver mi experiencia última';
-  /* Grab our button */
-  btn = document.querySelector('.btn-experiencia');
-  
-  /* Add a listener to the button instance so we can manipulate it */
-  btn.addEventListener('click', function() {
-    changeText(this, defaultText, substituteText);
-  }, false);
-  
+  const changeText = document.querySelector("#btn-experiencia");
+
+  changeText.addEventListener("click", function() {
+  changeText.textContent = "Ver mi experiencia última";
+});
+
+function textRevert() {
+  var textRevert = document.querySelector("#btn-experiencia");
+  if (textRevert.textContent = "Ver mi experiencia última") {
+    textRevert.addEventListener("click", function() {
+      textRevert.textContent = "Ver mi experiencia anterior";
+  });
+}
+}
