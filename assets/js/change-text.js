744e451f570f71136189edc639ef9e9fd5e22a76
l.a.senderovsky <l.a.senderovsky@gmail.com>
2024-05-30 22:24:10 +0000

    cambios js change text

--- assets/js/change-text.js
+++ assets/js/change-text.js
@@ -4,11 +4,9 @@
   changeText.textContent = "Ver mi experiencia última";
 });
 
-function textRevert() {
-  var textRevert = document.querySelector("#btn-experiencia");
-  if (textRevert.textContent = "Ver mi experiencia última") {
-    textRevert.addEventListener("click", function() {
-      textRevert.textContent = "Ver mi experiencia anterior";
-  });
-}
+
+if (changeText.textContent = "Ver mi experiencia última") {
+  changeText.addEventListener ("click", function() {
+  changeText.textContent = "Ver mi experiencia anterior";
+});
 }
