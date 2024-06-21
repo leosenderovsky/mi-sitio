18eaec0aac7d4511b4f47b51385c6f7a7b2b625a
l.a.senderovsky <l.a.senderovsky@gmail.com>
2024-06-21 14:48:17 +0000

    cambios varios de diseño

--- assets/js/change-text.js
+++ assets/js/change-text.js
@@ -1,12 +1,22 @@
-  const changeText = document.querySelector("#btn-experiencia");
+  /*const changeText = document.querySelector("#btn-experiencia");
 
   changeText.addEventListener("click", function() {
   changeText.textContent = "Ver mi experiencia última";
 });
 
 
-if (changeText.textContent = "Ver mi experiencia última") {
+if (changeText.textContent == "Ver mi experiencia última") {
   changeText.addEventListener ("click", function() {
   changeText.textContent = "Ver mi experiencia anterior";
 });
-}
+}*/
+
+
+const changeText = document.querySelector("#btn-experiencia");
+changeText.addEventListener("click", function() {
+  if (changeText.textContent === "Ver mi experiencia anterior") {
+    changeText.textContent = "Ver mi experiencia última";
+  } else {
+    changeText.textContent = "Ver mi experiencia anterior";
+  }
+});
