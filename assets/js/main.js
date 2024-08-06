1b713ae67105e6d43cab9eaee1c526bdd3b1d4b7
l.a.senderovsky <l.a.senderovsky@gmail.com>
2024-08-06 13:31:35 +0000

    corregido boton toggler del menu que tardaba en cargar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,173 +1,167 @@
-(function($) {
-  
-  "use strict";  
+(function ($) {
+  "use strict";
 
-  $(window).on('load', function() {
-
+  $(function () {
     /* 
    MixitUp
    ========================================================================== */
-  $('#portfolio').mixItUp();
+    $("#portfolio").mixItUp();
 
-  /* 
+    /* 
    One Page Navigation & wow js
    ========================================================================== */
-    var OnePNav = $('.onepage-nev');
+    var OnePNav = $(".onepage-nev");
     var top_offset = OnePNav.height() - -0;
     OnePNav.onePageNav({
-      currentClass: 'active',
+      currentClass: "active",
       scrollOffset: top_offset,
     });
-  
-  /*Page Loader active
-    ========================================================*/
-    $('#preloader').fadeOut();
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 200) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
+    /*Page Loader active
+    ========================================================*/
+    $("#preloader").fadeOut();
+
+    // Sticky Nav
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() > 200) {
+        $(".scrolling-navbar").addClass("top-nav-collapse");
+      } else {
+        $(".scrolling-navbar").removeClass("top-nav-collapse");
+      }
     });
 
     /* slicknav mobile menu active  */
-    $('.mobile-menu').slicknav({
-        prependTo: '.navbar-header',
-        parentTag: 'liner',
-        allowParentLinks: true,
-        duplicate: true,
-        label: '',
-        closedSymbol: '<i class="icon-arrow-right"></i>',
-        openedSymbol: '<i class="icon-arrow-down"></i>',
-      });
+    $(".mobile-menu").slicknav({
+      prependTo: ".navbar-header",
+      parentTag: "liner",
+      allowParentLinks: true,
+      duplicate: true,
+      label: "",
+      closedSymbol: '<i class="icon-arrow-right"></i>',
+      openedSymbol: '<i class="icon-arrow-down"></i>',
+    });
 
-      /* WOW Scroll Spy
+    /* WOW Scroll Spy
     ========================================================*/
-     var wow = new WOW({
+    var wow = new WOW({
       //disabled for mobile
-        mobile: false
+      mobile: false,
     });
 
     wow.init();
 
     /* Nivo Lightbox 
     ========================================================*/
-    $('.lightbox').nivoLightbox({
-        effect: 'fadeScale',
-        keyboardNav: true,
-      });
+    $(".lightbox").nivoLightbox({
+      effect: "fadeScale",
+      keyboardNav: true,
+    });
 
     /* Counter
     ========================================================*/
-    $('.counterUp').counterUp({
-     delay: 10,
-     time: 1000
+    $(".counterUp").counterUp({
+      delay: 10,
+      time: 1000,
     });
-
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > offset) {
+        $(".back-to-top").fadeIn(400);
+      } else {
+        $(".back-to-top").fadeOut(400);
+      }
+    });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+    $(".back-to-top").on("click", function (event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: 0,
+        },
+        600
+      );
+      return false;
+    });
+  });
 
-
-
-  }); 
-  
   // Form
-	var contactForm = function() {
-		if ($('#contactForm').length > 0 ) {
-			$( "#contactForm" ).validate( {
-				rules: {
-					name: "required",
-					subject: "required",
-					email: {
-						required: true,
-						email: true
-					},
-					message: {
-						required: true,
-						minlength: 5
-					}
-				},
-				messages: {
-					name: "Please enter your name",
-					subject: "Please enter your subject",
-					email: "Please enter a valid email address",
-					message: "Please enter a message"
-				},
-				/* submit via ajax */
-				
-				submitHandler: function(form) {		
-					var $submit = $('.submitting'),
-						waitText = 'Submitting...';
+  var contactForm = function () {
+    if ($("#contactForm").length > 0) {
+      $("#contactForm").validate({
+        rules: {
+          name: "required",
+          subject: "required",
+          email: {
+            required: true,
+            email: true,
+          },
+          message: {
+            required: true,
+            minlength: 5,
+          },
+        },
+        messages: {
+          name: "Please enter your name",
+          subject: "Please enter your subject",
+          email: "Please enter a valid email address",
+          message: "Please enter a message",
+        },
+        /* submit via ajax */
 
-					$.ajax({   	
-				      type: "POST",
-				      url: "php/sendEmail.php",
-				      data: $(form).serialize(),
+        submitHandler: function (form) {
+          var $submit = $(".submitting"),
+            waitText = "Submitting...";
 
-				      beforeSend: function() { 
-				      	$submit.css('display', 'block').text(waitText);
-				      },
-				      success: function(msg) {
-		               if (msg == 'OK') {
-		               	$('#form-message-warning').hide();
-				            setTimeout(function(){
-		               		$('#contactForm').fadeIn();
-		               	}, 1000);
-				            setTimeout(function(){
-				               $('#form-message-success').fadeIn();   
-		               	}, 1400);
+          $.ajax({
+            type: "POST",
+            url: "php/sendEmail.php",
+            data: $(form).serialize(),
 
-		               	setTimeout(function(){
-				               $('#form-message-success').fadeOut();   
-		               	}, 8000);
+            beforeSend: function () {
+              $submit.css("display", "block").text(waitText);
+            },
+            success: function (msg) {
+              if (msg == "OK") {
+                $("#form-message-warning").hide();
+                setTimeout(function () {
+                  $("#contactForm").fadeIn();
+                }, 1000);
+                setTimeout(function () {
+                  $("#form-message-success").fadeIn();
+                }, 1400);
 
-		               	setTimeout(function(){
-				               $submit.css('display', 'none').text(waitText);  
-		               	}, 1400);
+                setTimeout(function () {
+                  $("#form-message-success").fadeOut();
+                }, 8000);
 
-		               	setTimeout(function(){
-		               		$( '#contactForm' ).each(function(){
-											    this.reset();
-											});
-		               	}, 1400);
-			               
-			            } else {
-			               $('#form-message-warning').html(msg);
-				            $('#form-message-warning').fadeIn();
-				            $submit.css('display', 'none');
-			            }
-				      },
-				      error: function() {
-				      	$('#form-message-warning').html("Something went wrong. Please try again.");
-				         $('#form-message-warning').fadeIn();
-				         $submit.css('display', 'none');
-				      }
-			      });    		
-		  		} // end submitHandler
+                setTimeout(function () {
+                  $submit.css("display", "none").text(waitText);
+                }, 1400);
 
-			});
-		}
-	};
-	contactForm();
-
-}(jQuery));
+                setTimeout(function () {
+                  $("#contactForm").each(function () {
+                    this.reset();
+                  });
+                }, 1400);
+              } else {
+                $("#form-message-warning").html(msg);
+                $("#form-message-warning").fadeIn();
+                $submit.css("display", "none");
+              }
+            },
+            error: function () {
+              $("#form-message-warning").html("Something went wrong. Please try again.");
+              $("#form-message-warning").fadeIn();
+              $submit.css("display", "none");
+            },
+          });
+        }, // end submitHandler
+      });
+    }
+  };
+  contactForm();
+})(jQuery);
